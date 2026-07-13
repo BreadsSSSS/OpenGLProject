ca50ae9bf989a036db7506301646225ce3ba9a13
nowaljin <tcapg22603019@gmail.com>
2026-07-13 17:47:52 +0900

    FirstPush

---
+++ MyOpenGLProject.cpp
@@ -0,0 +1,40 @@
+﻿#define STB_IMAGE_IMPLEMENTATION
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <stb_image.h>
+#include <iostream>
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", NULL , NULL);
+    if (!window) {
+        std::cout << "窗口创建失败" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "GLAD初始化失败" << std::endl;
+        return -1;
+    }
+
+    // 测试GLM
+    glm::vec3 pos(1.0f, 2.0f, 3.0f);
+    std::cout << "GLM正常：" << pos.x << std::endl;
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
